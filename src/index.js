3eb9fb0335b6d053dc38b0ec11a78f89d0eb6df2
Yury Staravoitau <MorfinAll@mail.ru>
2018-03-02 13:41:42 +0300

    98/100 tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  //console.log("number= " + number);
+
+  function getSimpleMultipliers(num) {
+    let i = 2;
+    let index = 0;
+    let tempNum = num;
+    let result = [];
+    while (i <= num) {
+      if (tempNum % i === 0) {
+        result.push({ divider: i, summ: 0 });
+        index++;
+        tempNum = tempNum / i;
+      }
+      else {
+        i++;
+      }
+    }
+    //console.log(result);
+    return result;
+  }
+
+  let arr_multi = getSimpleMultipliers(base);
+
+  let maxDivider = arr_multi[arr_multi.length - 1].divider;
+  //console.log("maxDivider= " + maxDivider);
+
+  let zeroes = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let temp_i = i;
+    while (temp_i % maxDivider === 0) {
+      temp_i = Math.abs(temp_i / maxDivider);
+      zeroes++
+    }
+  }
+
+  let count_of_repeating_max_divider = 0;
+  arr_multi.forEach(function (element) {
+    if (maxDivider === element.divider) {
+      count_of_repeating_max_divider++;
+    };
+  })
+
+  zeroes = Math.floor(zeroes / count_of_repeating_max_divider);
+
+  return zeroes;
 }
