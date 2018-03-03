156a57fe7c3bd4c6692e0e7ae754cffe20305ae3
Yury Staravoitau <MorfinAll@mail.ru>
2018-03-03 10:13:38 +0300

    Added checker expetion when we need orient to lower divider

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ module.exports = function getZerosCount(number, base) {
     let maxDivider = 0;
     while (i <= num) {
       if (tempNum % i === 0) {
-        result.push({ divider: i, quantity: 1 });
+        result.push({ divider: i, quantity: 1, count: 0 });
         index++;
         tempNum = tempNum / i;
       }
@@ -23,7 +23,6 @@ module.exports = function getZerosCount(number, base) {
         i = i - 1;
       }
     }
-    //console.log(result);
     return result;
   }
 
@@ -31,6 +30,30 @@ module.exports = function getZerosCount(number, base) {
 
   let maxDivider = arr_multi[arr_multi.length - 1].divider;
   let maxDividerQuant = arr_multi[arr_multi.length - 1].quantity;
+
+  // for exeption when we need orient to lower divider chk this exeption MB this is not good on low numbers. Need tests this on low numbers
+  // ##########
+  for (let i = 2; i <= 100; i++) {
+    let not_next = true;
+    let temp_i = i;
+    let index = 0;
+    while (index < arr_multi.length) {
+      let element = arr_multi[index];
+      while (temp_i % element.divider === 0) {
+        element.count++
+        temp_i = temp_i / element.divider;
+      }
+      index++;
+    }
+  }
+
+  for (let i = 1; i < arr_multi.length; i++) {
+    if (arr_multi[i].count / arr_multi[i].quantity > arr_multi[i - 1].count / arr_multi[i - 1].quantity) {
+      maxDivider = arr_multi[i - 1].divider;
+      maxDividerQuant = arr_multi[i - 1].quantity;
+    }
+  }
+  // ##########
 
   let zeroes = 0;
 
